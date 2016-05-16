1ea144d8add1c46761dbaa25b6863f567f4f8fb4
sreekanth-anubolu <anubolusreekanth@gmail.com>
2016-05-16 13:31:37 +0530

    Minor fix

--- food_suggest/settings.py
+++ food_suggest/settings.py
@@ -136,6 +136,6 @@ STATICFILES_DIRS = [
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'anubolusreekanth@gmail.com'
-EMAIL_HOST_PASSWORD = 'shivashivashivashiva'
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''
 
